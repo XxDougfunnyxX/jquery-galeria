bd4e730ea29325b9f0a8bfdd1665e06f0c02a4e7
Douglas Ricardo <douglasricardo1995@gmail.com>
2024-05-21 12:10:34 -0300

    Jquery

--- main.js
+++ main.js
@@ -1,24 +1,18 @@
-$(document).ready(function(){
-    $('header button').click(function() {
-        $('form').slideDown();
-    })
+const form = document.getElementById("tarefa-form");
+let linhas = '';
 
-    $('#botao-cancelar').click(function() {
-        $('form').slideUp();
-     })
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
 
-    $('form').on('submit',function(e) {
-    e.preventDefault();
-    const enderecoDaNovaImagem =$('#endereco-imagem-nova').val();
-  const novoItem =$('<li style="display=none"></li>');
-  $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
-  $(`<div class="overlay-imagem-link">
-  <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real
-  </a>
-  </div>
-  `).appendTo(novoItem);
-  $(novoItem).appendTo('ul');
-  $(novoItem).fadeIn(1000);
-  $('#endereco-imagem-nova').val('')
-})
-})
+  const inputNomeTarefa = document.getElementById('task-form');
+
+  let linha = '<tr>';
+linha += `<td>${inputNomeTarefa.value}</td>`;
+linha += `</tr>`;
+
+linhas += linha;
+
+const corpoTabela = document.querySelector('tbody');
+corpoTabela.innerHTML = linhas;
+  
+});
